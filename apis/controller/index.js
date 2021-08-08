83e7ee0c102e5dbe543659a840b3f0a9066dd175
Rohit Patil <rohit3.patil@ril.com>
2021-08-09 01:21:55 +0530

    adding state wise api

--- apis/controller/index.js
+++ apis/controller/index.js
@@ -96,15 +96,86 @@ const getDateInfo = async (req, res) => {
 
 const getStateInfo = async (req, res) => {
     try {
-        let result = await fetch(config.getStateInfoUrl, {
-            method: 'post',
-            body: JSON.stringify(req.body),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        resutl = await result.json();
+        let { State } = req.body;
+        const promiseArray = [
+            covidCount.aggregate([
+                {
+                    $match: {
+                        "State/UnionTerritory": State
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$State/UnionTerritory",
+                        ConfirmedIndianNational: { $sum: "$ConfirmedIndianNational" },
+                        ConfirmedForeignNational: { $sum: "$ConfirmedForeignNational" },
+                        Deaths: { $sum: "$Deaths" },
+                        Cured: { $sum: "$Cured" },
+                        Confirmed: { $sum: "$Confirmed" }
+                    }
+                },
+                {
+                    $project: {
+                        Date: 1,
+                        State: 1,
+                        ConfirmedIndianNational: 1,
+                        ConfirmedForeignNational: 1,
+                        Deaths: 1,
+                        Cured: 1,
+                        Confirmed: 1
+                    }
+                }
+            ]),
+            stateTest.aggregate([
+                {
+                    $match: {
+                        State
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$State",
+                        TotalSamples: { $sum: "$TotalSamples" },
+                        Negative: { $sum: "$Negative" },
+                        Positive: { $sum: "$Positive" },
+                    }
+                },
+                {
+                    $project: {
+                        State: 1,
+                        Negative: 1,
+                        Positive: 1,
+                    }
+                }
+            ]),
+            vaccines.aggregate([
+                {
+                    $match: {
+                        State
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$State",
+                        'Total Doses Administered': { $sum: "$Total Doses Administered" },
+                    }
+                },
+                {
+                    $project: {
+                        State: 1,
+                        'Total Doses Administered': 1,
+                    }
+                }
+            ])
+        ];
+        let result = await Promise.all(promiseArray);
         return res.status(200).send({
             code: 200,
-            result
+            result: {
+                covidStats: (result[0] && result[0].length) > 0 ? result[0] : [],
+                stateWiseStats: (result[1] && result[1].length) > 0 ? result[1] : [],
+                vaccineStats: (result[2] && result[2].length) > 0 ? result[2] : [],
+            }
         });
     } catch (err) {
         return res.status(500).send(err);
