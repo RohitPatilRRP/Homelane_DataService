374074939e068bb708edae82b9f9ec7bcff77a1a
Rohit Patil <rohit3.patil@ril.com>
2021-08-09 01:37:23 +0530

    Adding pinpoint api

--- apis/controller/index.js
+++ apis/controller/index.js
@@ -184,15 +184,98 @@ const getStateInfo = async (req, res) => {
 
 const getPinpointInfo = async (req, res) => {
     try {
-        let result = await fetch(config.getPinpointInfoUrl, {
-            method: 'post',
-            body: JSON.stringify(req.body),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        resutl = await result.json();
+        let { State, Date: date } = req.body;
+        let updateDate = moment(new Date(date)).format('DD/MM/YYYY');
+        const promiseArray = [
+            covidCount.aggregate([
+                {
+                    $match: {
+                        $and: [
+                            { "State/UnionTerritory": State },
+                            { Date: date }
+                        ]
+                    }
+                },
+                {
+                    $group: {
+                        _id: { "State": "$State/UnionTerritory", "Date": "$Date" },
+                        ConfirmedIndianNational: { $sum: "$ConfirmedIndianNational" },
+                        ConfirmedForeignNational: { $sum: "$ConfirmedForeignNational" },
+                        Deaths: { $sum: "$Deaths" },
+                        Cured: { $sum: "$Cured" },
+                        Confirmed: { $sum: "$Confirmed" }
+                    }
+                },
+                {
+                    $project: {
+                        Date: 1,
+                        State: 1,
+                        ConfirmedIndianNational: 1,
+                        ConfirmedForeignNational: 1,
+                        Deaths: 1,
+                        Cured: 1,
+                        Confirmed: 1
+                    }
+                }
+            ]),
+            stateTest.aggregate([
+                {
+                    $match: {
+                        $and: [
+                            { State },
+                            { Date: date }
+                        ]
+                    }
+                },
+                {
+                    $group: {
+                        _id: { "State": "$State", "Date": "$Date" },
+                        TotalSamples: { $sum: "$TotalSamples" },
+                        Negative: { $sum: "$Negative" },
+                        Positive: { $sum: "$Positive" },
+                    }
+                },
+                {
+                    $project: {
+                        State: 1,
+                        Date:1,
+                        Negative: 1,
+                        Positive: 1,
+                    }
+                }
+            ]),
+            vaccines.aggregate([
+                {
+                    $match: {
+                        $and: [
+                            { State },
+                            { 'Updated On': updateDate }
+                        ]
+                    }
+                },
+                {
+                    $group: {
+                        _id: { "State": "$State", "Date": "$Date" },
+                        'Total Doses Administered': { $sum: "$Total Doses Administered" },
+                    }
+                },
+                {
+                    $project: {
+                        State: 1,
+                        'Updated On':1,
+                        'Total Doses Administered': 1,
+                    }
+                }
+            ])
+        ];
+        let result = await Promise.all(promiseArray);
         return res.status(200).send({
             code: 200,
-            result
+            result: {
+                covidStats: (result[0] && result[0].length) > 0 ? result[0] : [],
+                stateWiseStats: (result[1] && result[1].length) > 0 ? result[1] : [],
+                vaccineStats: (result[2] && result[2].length) > 0 ? result[2] : [],
+            }
         });
     } catch (err) {
         return res.status(500).send(err);
